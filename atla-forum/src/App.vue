117b548f384492dc8a418131170fb718618a33de
jolly6 <98voonfusing@gmail.com>
2023-03-20 07:48:24 +0800

    Refactor: removed boilerplate components in atla-forum

--- atla-forum/src/App.vue
+++ atla-forum/src/App.vue
@@ -1,47 +1,103 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        <h2>Statistics</h2>
+        <ul>
+            <li v-for="(stat, key) in benderStatistics" :key="`bender-${stat}-${key}`">
+                {{key}}: {{stat}}
+            </li>
+        </ul>
+        <h2>Characters</h2>
+        <p v-if="characters.length === 0">There are no characters</p>
+        <ul v-else-if="characters.length % 2 ===0">
+            <li v-for="(character, index) in characters" :key="`even-character-${index}`">
+                <p>{{character.name}}</p>
+                <button @click="favoriteCharacter(character)">Favorite</button>
+            </li>
+        </ul>
+        <p v-else>There are odd number of characters</p>
+        <h2>Favorite Characters</h2>
+        <ul v-if="favoriteList.length > 0">
+            <li v-for="(character, index) in favoriteList" :key="`favorite-${index}`">
+                {{character.name}}
+            </li>
+        </ul>
+        <p v-else>No favorite characters</p>
+        <h2>Add new character</h2>
+        <label for="character-name">Name</label>
+        <pre>{{newCharacter}}</pre>
+        <input 
+        type="text" 
+        v-model="newCharacter.name" 
+        @keyup.enter="addNewCharacter"
+        >
+        <p>
+            <span v-for="(character, index) in characters" :key="`character-${index}`">
+                {{character.name}}{{index === characters.length - 1 ? '' : ', '}}
+            </span>
+        </p>
     </div>
-  </header>
 
   <main>
     <TheWelcome />
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  data: () => ({
+            newCharacter: {
+                name: '',
+                element: []
+            },
+            characters: [
+            {
+              name: 'Aang',
+              element: ['Air', 'Water', 'Fire', 'Earth']
+            },
+            {
+              name: 'Zuko',
+              element: ['Fire']
+            },
+            {
+              name: 'Toph',
+              element: ['Earth']
+            },
+            {
+              name: 'Katara',
+              element: ['Water']
+            }
+          ],
+          favoriteList: []
+        }),
+        computed: {
+            benderStatistics() {
+                const elements = ['Air', 'Water', 'Fire', 'Earth']
+                const statistics = {
+                    'Air': 0,
+                    'Water': 0,
+                    'Fire': 0,
+                    'Earth': 0
+                }
+
+                this.characters.forEach(character => {
+                    elements.forEach(element => {
+                        if (character.element.indexOf(element) > -1) {
+                            statistics[element] += 1
+                        }
+                    })
+                })
+
+                return statistics 
+            }
+        },
+        methods: {
+            addNewCharacter() {
+                this.characters.push(this.newCharacter)
+                this.newCharacter = {name:''}
+            },
+            favoriteCharacter(character) {
+                this.favoriteList.push(character)
+            }
+        }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,14 +1,26 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <h1>{{ displayTitle }}</h1>
+      <p :data-increment-by="incrementAmount">{{ count }}</p>
+      <button @click="incrementCount">Increment Count</button>
+      <h1>{{ incrementAmount }}</h1>
+      <p>{{ optimizedIncrementAmount }}</p>
+      <div>
+        <label for="incrementAmount">Increment by:</label>
+        <input type="text" v-model="incrementAmount" />
+      </div>
+      <hr />
+      <p v-if="message.length % 2 === 0">Even: {{ message.toUpperCase() }}</p>
+      <p v-else>Odd: {{ message }}</p>
+      <ul v-for="(item, index) in listOfNumbers" :key="`item-${index}`">
+        <li>
+          {{ item.id }}
+          <ul>
+            <li v-for="(number, index) in item.list" :key="`number-${index}`">{{ number }}</li>
+          </ul>
+        </li>
+      </ul>
     </div>
   </header>
 
@@ -17,31 +29,69 @@ import TheWelcome from './components/TheWelcome.vue'
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+export default {
+  data() {
+          return {
+            count: 10,
+            counterTitle: 'Counter Standard',
+            incrementAmount: 8,
+            message: 'Hello it works',
+            listOfNumbers: [
+              {
+                name: 1,
+                id: '6a887cd2-f0bf-4321-b192-92016f82a883',
+                list: [1, 2, 3]
+              },
+              {
+                name: 2,
+                id: '8d14d90b-2d47-473e-8293-d5c324111d0d',
+                list: [1, 2, 3]
+              },
+              {
+                name: 3,
+                id: 'cd806d65-2309-4625-9104-dcd636cd79b5',
+                list: [1, 2, 3]
+              },
+              {
+                name: 4,
+                id: '00c939cd-fbf4-46d0-8e61-0bc2ce8a5332',
+                list: [1, 2, 3]
+              },
+              {
+                name: 5,
+                id: '295a8170-59c1-4462-9de1-3c9cd41cedab',
+                list: [1, 2, 3]
+              }
+            ]
+          }
+        },
+        computed: {
+          displayTitle() {
+            if (this.count > 20) {
+              return 'Counter Standard - Very Long'
+            } else {
+              return 'Counter Standard'
+            }
+          },
+          optimizedIncrementAmount() {
+            return this.displayTitle.length * this.incrementAmount
+          }
+        },
+        methods: {
+          incrementCount(newAmount, event) {
+            console.log(newAmount)
+            console.log(event)
+            this.count += this.optimizedIncrementAmount
+          }
+        },
+        watch: {
+          count(newValue) {
+            if (newValue > 20) {
+              this.counterTitle += ' Very Long'
+            }
+          }
+        }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
