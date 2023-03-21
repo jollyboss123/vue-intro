0dcbee01a55678b05377127a146f7a8afac31c72
jolly6 <98voonfusing@gmail.com>
2023-03-22 07:35:32 +0800

    Added Pokedex.vue and introduced setup() and Suspense in sandbox

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,29 +1,20 @@
 <template>
   <h1>New App</h1>
-  <pre>{{ pokedex }}</pre>
-  <button @click="fetchPokemon">Fetch Pokemon</button>
+  <Suspense>
+    <Pokedex />
+
+    <template v-slot:fallback>
+      Loading Pokedex...
+    </template>
+    </Suspense>
+  
 </template>
 
 <script>
-//https://pokeapi.co/api/v2/pokemon?limit=151
+import Pokedex from './components/Pokedex.vue';
 export default {
-  data: () => ({
-    pokedex: [1,2,3],
-  }),
-  methods: {
-    async fetchPokemon() {
-      this.pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((response) => response.json())
-    }
-  },
-  beforeCreate() {
-    console.log("Before Created");
-    console.log(this.pokedex);
-  },
-  created() {
-    console.log("Created");
-    console.log(this.pokedex);
-    this.fetchPokemon();
+  components: {
+    Pokedex
   }
 }
 </script>
---
+++ sandbox/src/components/Pokedex.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    async setup() {
+        // only vanilla js in setup 
+        const regionName = "kanto"
+
+        const pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+        .then((response) => response.json())
+
+        return {
+            pokedex,
+            regionName
+        }
+    },
+    beforeCreate() {
+        console.log("Before Created");
+        console.log(this.pokedex);
+        console.log(this.regionName);
+    },
+    created() {
+        console.log("Created");
+        console.log(this.pokedex);
+        console.log(this.regionName);
+    }
+}
+</script>
+
+<template>
+    <pre>{{ pokedex }}</pre>
+</template>
