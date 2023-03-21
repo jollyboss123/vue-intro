d51af236f88db289e8774932fedf41977324996d
jolly6 <98voonfusing@gmail.com>
2023-03-21 22:03:45 +0800

    Added fetch data functionality asynchronously in sandbox

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <h1>New App</h1>
+  <pre>{{ pokedex }}</pre>
+  <button @click="fetchPokemon">Fetch Pokemon</button>
 </template>
 
 <script>
+//https://pokeapi.co/api/v2/pokemon?limit=151
 export default {
-
+  data: () => ({
+    pokedex: [1,2,3],
+  }),
+  methods: {
+    async fetchPokemon() {
+      this.pokedex = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+      .then((response) => response.json())
+    }
+  }
 }
 </script>
