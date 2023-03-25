7f0d313f02463440b7cf8bbd5a2f382b51847f13
jolly6 <98voonfusing@gmail.com>
2023-03-25 17:41:00 +0800

    Introduced reactive css element in sandbox

--- sandbox/src/App.vue
+++ sandbox/src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <BaseCounter />
-  <UserCard :user="{ name: 'Ben', food: 'Ramen' }"/>
+  <div class="wrapper">
+    <h2>{{ colorPreference }}</h2>
+    <input type="color" v-model="colorPreference"/>
+    <BaseCounter />
+    <UserCard :user="{ name: 'Ben', food: 'Ramen' }"/>
+  </div>
   
 </template>
 
 <script setup>
 import BaseCounter from './components/BaseCounter.vue';
 import UserCard from './components/UserCard.vue';
-  
+import { ref } from 'vue';
+
+const colorPreference = ref("black")
 </script>
 
 <style>
 html {
   background-color: aliceblue;
+}
+
+.wrapper {
+  background-color: v-bind(colorPreference);
 }
 
 .button {
